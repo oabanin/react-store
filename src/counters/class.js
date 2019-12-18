355d35a6f06d72100b4678c42f663c5622037170
Oabanin <juristaba@gmail.com>
2019-12-18 23:37:50 +0300

    Revert to No binding and no constructor

--- src/counters/class.js
+++ src/counters/class.js
@@ -3,18 +3,7 @@ import React from 'react';
 export default class extends React.Component{
  
 
-    constructor(props){
-        super(props);
 
-        this.state = {
-            cnt: 1
-        }
-    
-        this.increase = () => {
-            console.log(this.state);
-            //this.state.cnt++; //Do not work
-        }
-    }
 
     state = {
         cnt: 1
