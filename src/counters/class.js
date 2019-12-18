6be154dd00d6a7f907207dce372dda90d66e0ec9
Oabanin <juristaba@gmail.com>
2019-12-18 23:37:08 +0300

    Same as previous commit

--- src/counters/class.js
+++ src/counters/class.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 export default class extends React.Component{
  
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            cnt: 1
+        }
+    
+        this.increase = () => {
+            console.log(this.state);
+            //this.state.cnt++; //Do not work
+        }
+    }
+
     state = {
         cnt: 1
     }
