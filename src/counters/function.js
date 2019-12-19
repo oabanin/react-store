cc2198e9f5d21c8f17b1aa1ba31cd7645580d578
oabanin-global-name <juristaba@gmail.com>
2019-12-19 12:12:02 +0400

    Add random state to dunction.js

--- src/counters/function.js
+++ src/counters/function.js
@@ -3,17 +3,19 @@ import React, {useState} from 'react';
 export default function () {
 
     let [cnt,setCnt] = useState(0);
+    let [randCnt,randSetCnt] = useState(10);
 
     let increase = () => {
         setCnt(cnt+1);
+        randSetCnt(Math.random());
     }
 
     return (
         <div>
             <strong>{cnt}</strong>
             <br />
+            Random state: {randCnt}<br/>
             <button onClick={increase}>+1</button>
-
         </div>
     );
 
