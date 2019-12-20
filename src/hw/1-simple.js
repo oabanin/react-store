51b472cd805cda1217931107a726698bed42fc0a
oabanin-WORK <juristaba@gmail.com>
2019-12-20 17:03:13 +0400

    Added decrease and increase by dima

--- src/hw/1-simple.js
+++ src/hw/1-simple.js
@@ -7,21 +7,23 @@ export default class extends React.Component {
     }
 
     increase = () => {
-        this.setState({
-            cnt: this.state.cnt + 1
-        },()=>{
-            this.setState({
-                cnt: this.state.cnt + 1
-            })
-        })
 
+        let cnt = this.state.cnt + 1;
+
+        if (cnt > this.props.max) {
+            cnt = this.props.max;
+        }
+        this.setState({cnt});
     }
 
     decrease = () => {
-        console.log(1);
-        this.setState({
-            cnt: this.state.cnt - 1
-        })
+
+        let cnt = this.state.cnt - 1;
+        
+        if (cnt < this.props.min) {
+            cnt = this.props.min;
+        }
+        this.setState({cnt});
     }
     render() {
         return (
