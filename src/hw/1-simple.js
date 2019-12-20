cec5d14fccd76c0eb7fbcbfb8e7e7e292afeee20
oabanin-WORK <juristaba@gmail.com>
2019-12-20 10:51:36 +0400

    setState is async and works only onetime

---
+++ src/hw/1-simple.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default class extends React.Component {
+
+    state = {
+        cnt: this.props.min
+    }
+
+    increase = () => {
+        this.setState({
+            cnt: this.state.cnt + 1
+        })
+
+        this.setState({
+            cnt: this.state.cnt + 1
+        })
+
+
+        this.setState({
+            cnt: this.state.cnt + 1
+        })
+
+        this.setState({
+            cnt: this.state.cnt + 1
+        })
+    }
+
+    decrease = () => {
+        console.log(1);
+        this.setState({
+            cnt: this.state.cnt - 1
+        })
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.decrease}>-</button>
+                <strong>{this.state.cnt}</strong>
+                <button onClick={this.increase}>+</button>
+
+            </div>
+        );
+    }
+}
