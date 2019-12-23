8c76838a84c9c6efd4e53273b5156adf4c2add2c
oabanin-WORK <juristaba@gmail.com>
2019-12-23 15:55:37 +0400

    Add lazy input

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import HW1 from './hw/1-simple';
 import HW2 from './hw/2-input';
-
+import HW3 from './hw/3-lazy';
 
 export default function(){
     return(
@@ -9,7 +9,9 @@ export default function(){
         <h2>MinMax Simple</h2>
         <HW1 min={1} max={10}/>
         <h2>MinMax input</h2>
-        <HW2 min={1} max={10}/>
+        <HW2 min={-11} max={10}/>
+        <h2>LAZY input</h2>
+        <HW3 min={5} max={10}/>
     </div>
 
     );
---
+++ src/hw/3-lazy.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class extends React.Component {
+
+    static propTypes = {
+        min: PropTypes.number.isRequired,
+        max: PropTypes.number.isRequired
+    }
+
+    state = {
+        cnt: this.props.min,
+        inputValue : this.props.min
+    }
+
+    increase = () => {
+        this.set(this.state.cnt + 1);
+    }
+
+    decrease = () => {
+        this.set(this.state.cnt - 1);
+    }
+
+    set(newCnt) {
+        let cnt = Math.max(this.props.min, Math.min(newCnt, this.props.max));
+        this.setState({ cnt });
+    }
+
+    setValue(newStr){
+        this.setState({ inputValue : newStr })
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.decrease}>-</button>
+                <input value={this.state.cnt}
+                       onChange={(e) => this.setValue(e.target.value)} />
+                <button onClick={this.increase}>+</button>
+            </div>
+        );
+    }
+}
+/*
+Some.defaultProps = {
+    min: 1,
+    max: 5
+}
+*/
