49d9c792c0f23346f35b8e67048e7ad021f9fdf1
oabanin-HOME <juristaba@gmail.com>
2019-12-25 00:36:32 +0300

    add derived

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import HW1 from './hw/1-simple';
 import HW2 from './hw/2-input';
 import HW3 from './hw/3-lazy';
+import Derived from './hw/4-deriveds';
 
 export default function(){
     let [someMin, setSomeMin] = useState(20);
@@ -16,7 +17,9 @@ export default function(){
         <h2>MinMax input</h2>
         <HW2 min={-11} max={10}/>
         <h2>LAZY input</h2>
-        <HW3 min={someMin} max={100}/>
+        <HW3 min={20} max={100}/>
+        <h2>Derived Input</h2>
+        <Derived min={20} max={100}/>
     </div>
 
     );
---
+++ src/hw/4-deriveds.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class extends React.Component {
+
+    static propTypes = {
+        min: PropTypes.number.isRequired,
+        max: PropTypes.number.isRequired
+    }
+
+    state = {
+        cnt: this.props.min,
+        inputValue : this.props.min
+    }
+
+    increase = () => {
+        this.set(this.state.cnt + 1);
+    }
+
+    decrease = () => {
+        this.set(this.state.cnt - 1);
+    }
+
+    set(newCnt) {
+        let cnt = Math.max(this.props.min, Math.min(newCnt, this.props.max));
+        this.setState({ cnt, inputValue: cnt });
+    }
+
+    setValue(newStr){
+        this.setState({ inputValue : newStr })
+
+    }
+
+
+    appyValue = () => {
+        let cnt = parseInt(this.state.inputValue);
+        this.set(isNaN(cnt) ? this.state.min : cnt);
+    }
+
+    checkEnterKey = (e) => {
+        if(e.keyCode === 13){
+            this.appyValue();
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.min} - props.min <br/>
+            {this.state.cnt} - state.cnt <br/>
+            {this.state.inputValue} - state.inputValue<br/>
+                <button onClick={this.decrease}>-</button>
+                <input value={this.state.inputValue}
+                       onChange={(e) => this.setValue(e.target.value)}
+                       onBlur={this.appyValue}
+                       onKeyUp={this.checkEnterKey} />
+                <button onClick={this.increase}>+</button>
+            </div>
+        );
+    }
+}
+/*
+Some.defaultProps = {
+    min: 1,
+    max: 5
+}
+*/
