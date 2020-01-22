ff0f4890c5dbb8d39c21f6d10d8bbc655f9edb0d
oabanin-HOME <juristaba@gmail.com>
2020-01-22 19:06:03 +0300

    add withrouter from 'react-router-dom' to button

--- src/components/links/button/index.js
+++ src/components/links/button/index.js
@@ -1,7 +1,9 @@
  import React from 'react';
+import {withRouter} from 'react-router-dom';
+
  function LinkButton (props){
      console.log(props);
     return <button {...props} />
 }
 
-export default LinkButton;
+export default withRouter(LinkButton);
