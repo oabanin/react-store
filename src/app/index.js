e9a8428fbfb1a9ded38b3d58e6075de43cf5cf52
oabanin-HOME <juristaba@gmail.com>
2020-01-27 00:40:22 +0300

    replace Link with NavLink

--- src/app/index.js
+++ src/app/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import styles from './app.module.css';
 
 import { observer, Provider } from 'mobx-react'
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link, NavLink } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
 
 import routes, { routesMap } from '~/routes';
 
 import stores from '~s';
 
 export default @observer class extends React.Component {
+
+
     render() {
         let routesComponents = routes.map(route => {
             return <Route
@@ -17,26 +20,31 @@ export default @observer class extends React.Component {
                 exact={route.exact}
                 key={route.url}></Route>
         });
-
+        this.props
         return (
             <Provider stores={stores}>
                 <Router>
-                    
+
                     <div className="container">
-                    Number of products in cart: {stores.cart.productsDetailed.length}
+                        Number of products in cart: {stores.cart.productsDetailed.length}
 
                         <hr />
+
+
+
+
+
                         <div className="row">
                             <div className="col col-3">
                                 <ul className="list-group">
                                     <li className="list-group-item">
-                                        <Link to={routesMap.home}>Home</Link>
+                                        <NavLink exact to={routesMap.home} activeClassName="selected" to={routesMap.home}>Home</NavLink >
                                     </li>
                                     <li className="list-group-item">
-                                        <Link to={routesMap.cart}>Cart</Link>
+                                        <NavLink activeClassName="selected" to={routesMap.cart}>Cart</NavLink >
                                     </li>
                                     <li className="list-group-item">
-                                        <Link to={routesMap.order}>Order Now</Link>
+                                        <NavLink activeClassName="selected" to={routesMap.order}>Order Now</NavLink >
                                     </li>
                                 </ul>
                             </div>
