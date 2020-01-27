4ef2c03476e9e2cb07b6731886bb41f65c9647c4
oabanin-HOME <juristaba@gmail.com>
2020-01-27 14:05:57 +0300

    add selected style to NAvmenu

--- src/app/index.js
+++ src/app/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from './app.module.css';
 
 import { observer, Provider } from 'mobx-react'
-import { BrowserRouter as Router, Route, Switch, Link, NavLink } from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
+
 
 import routes, { routesMap } from '~/routes';
 
@@ -38,13 +38,13 @@ export default @observer class extends React.Component {
                             <div className="col col-3">
                                 <ul className="list-group">
                                     <li className="list-group-item">
-                                        <NavLink exact to={routesMap.home} activeClassName="selected" to={routesMap.home}>Home</NavLink >
+                                        <NavLink  activeClassName={styles.selected} to={routesMap.home} exact to={routesMap.home}>Home</NavLink >
                                     </li>
                                     <li className="list-group-item">
-                                        <NavLink activeClassName="selected" to={routesMap.cart}>Cart</NavLink >
+                                        <NavLink activeClassName={styles.selected} to={routesMap.cart}>Cart</NavLink >
                                     </li>
                                     <li className="list-group-item">
-                                        <NavLink activeClassName="selected" to={routesMap.order}>Order Now</NavLink >
+                                        <NavLink activeClassName={styles.selected} to={routesMap.order}>Order Now</NavLink >
                                     </li>
                                 </ul>
                             </div>
